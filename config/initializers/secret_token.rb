d94cd630a6a1f22ff32f9053050b8e65e42fa750
Mike Kovner <mike.kovner@gmail.com>
2013-08-26 10:19:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TccJsapiDemo::Application.config.secret_token = '8a1cbd561f63a947ccbb597dc1268c8e25a7967d75bf18f878ac205049c7229df243c1bf27c470e96272bb61ae088edd9934f1dbe6c6f7da3eb38f7f4911f448'
